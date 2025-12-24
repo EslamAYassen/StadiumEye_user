bdd1d54bfd129efef43ecb084d56ead25e45d874
abdou <abdouashrat123@gmail.com>
2025-12-24 15:55:09 +0200

    hotfix: fix some issues

--- lib/features/matches/presentation/pages/matches_page.dart
+++ lib/features/matches/presentation/pages/matches_page.dart
@@ -7,6 +7,7 @@ import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:stadium_eye/core/widgets/loading/lottie_loading.dart';
 
 import 'package:stadium_eye/features/matches/presentation/cubit/matches_cubit.dart';
+import 'package:stadium_eye/features/matches/presentation/widget/match_card.dart';
 import 'package:stadium_eye/theme/app_colors.dart';
 import 'package:stadium_eye/theme/app_theme_consts.dart';
 
@@ -14,36 +15,13 @@ import '../../../../constants/app_consts.dart';
 import '../../../../core/widgets/appbar_header/appbar_header.dart';
 import '../../../../core/widgets/drop_down/custom_dropdown.dart';
 import '../../../../l10n/app_localizations.dart';
-import '../../domain/entities/matches_res.dart';
+// import '../../domain/entities/matches_res.dart';
 import '../cubit/matches_state.dart';
 
-class MatchesPage extends StatefulWidget {
+class MatchesPage extends StatelessWidget {
   const MatchesPage({super.key});
 
-  @override
-  State<MatchesPage> createState() => _MatchesPageState();
-}
-
-class _MatchesPageState extends State<MatchesPage>
-    with TickerProviderStateMixin {
-  late Animation<Offset> _slide;
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 450),
-      vsync: this,
-    );
-
-    _slide = Tween<Offset>(
-      begin: const Offset(0, 0.2),
-      end: Offset.zero,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
-
-    super.initState();
-  }
-
+  // late Animation<Offset> _slide;
   @override
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
@@ -150,7 +128,10 @@ class _MatchesPageState extends State<MatchesPage>
           ),
 
           ...state.filteredMatches.map(
-            (match) => _buildMatchCard(context, match, isDarkMode),
+            (match) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MatchCard(match: match),
+            ),
           ),
         ],
       ),
@@ -454,275 +435,6 @@ class _MatchesPageState extends State<MatchesPage>
               date: DateTime.now().toIso8601String().split('T')[0],
             ),
             child: Text(AppLocalizations.of(context)!.retry),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildMatchCard(
-    BuildContext context,
-    Response match,
-    bool isDarkMode,
-    // Animation<Offset> slide,
-  ) {
-    return SlideTransition(
-      position: _slide,
-      child: Container(
-        margin: const EdgeInsets.only(bottom: AppThemeConsts.padding12sm),
-        padding: const EdgeInsets.all(AppThemeConsts.padding16md),
-        decoration: BoxDecoration(
-          color: isDarkMode
-              ? AppColors.darkGray.withAlpha((0.5 * 255) ~/ 1)
-              : AppColors.whiteColor,
-          borderRadius: BorderRadius.circular(AppThemeConsts.radius12md),
-          border: Border.all(
-            color: isDarkMode
-                ? AppColors.lightGray.withAlpha((0.1 * 255) ~/ 1)
-                : AppColors.lightGray,
-          ),
-          boxShadow: isDarkMode
-              ? []
-              : [
-                  BoxShadow(
-                    color: AppColors.blackColor.withAlpha((0.05 * 255) ~/ 1),
-                    blurRadius: 10,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
-        ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                _buildTeam(
-                  match.teams.home.name ?? '',
-                  match.teams.home.logo ?? '',
-                  true,
-                  isDarkMode,
-                ),
-                _buildMatchScore(match, isDarkMode),
-                _buildTeam(
-                  match.teams.away.name ?? '',
-                  match.teams.away.logo ?? '',
-                  false,
-                  isDarkMode,
-                ),
-              ],
-            ),
-            const SizedBox(height: AppThemeConsts.padding12sm),
-            _buildMatchInfo(match, isDarkMode),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTeam(String name, String logo, bool isHome, bool isDarkMode) {
-    return Expanded(
-      child: Column(
-        children: [
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              color: isDarkMode
-                  ? AppColors.lightGray.withAlpha((0.1 * 255) ~/ 1)
-                  : AppColors.lightGray,
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: isDarkMode
-                    ? AppColors.lightGray.withAlpha((0.2 * 255) ~/ 1)
-                    : AppColors.lightGray,
-                width: 2,
-              ),
-            ),
-            child: ClipOval(
-              child: Image.network(
-                logo,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Icon(
-                    Icons.sports_soccer,
-                    color: isDarkMode
-                        ? AppColors.whiteColor.withAlpha((0.5 * 255) ~/ 1)
-                        : AppColors.primary,
-                    size: 30,
-                  );
-                },
-              ),
-            ),
-          ),
-          const SizedBox(height: AppThemeConsts.padding8xs),
-          Text(
-            name,
-            textAlign: TextAlign.center,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              color: isDarkMode ? AppColors.whiteColor : AppColors.darkGray,
-              fontSize: 14,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildMatchScore(Response match, bool isDarkMode) {
-    final status = match.fixture.status.short;
-    final isLive =
-        status?.name == 'HT' || status?.name == '1H' || status?.name == '2H';
-    final bool isNotStarted = status?.name == 'NS' || status?.name == 'TBD';
-
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: AppThemeConsts.padding16md,
-        vertical: AppThemeConsts.padding8xs,
-      ),
-      child: Column(
-        children: [
-          if (isNotStarted)
-            Column(
-              children: [
-                Text(
-                  '-  -  -',
-                  style: TextStyle(
-                    color: isDarkMode
-                        ? AppColors.whiteColor.withAlpha((0.5 * 255) ~/ 1)
-                        : AppColors.darkGray.withAlpha((0.5 * 255) ~/ 1),
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  (match.fixture.date ?? DateTime.now())
-                      .toIso8601String()
-                      .split('T')[1]
-                      .substring(0, 5),
-                  style: TextStyle(
-                    color: isDarkMode
-                        ? AppColors.whiteColor.withAlpha((0.7 * 255) ~/ 1)
-                        : AppColors.darkGray,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
-            )
-          else
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  '${match.goals.home ?? 0}',
-                  style: TextStyle(
-                    color: isDarkMode
-                        ? AppColors.whiteColor
-                        : AppColors.darkGray,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  child: Text(
-                    '-',
-                    style: TextStyle(
-                      color: isDarkMode
-                          ? AppColors.whiteColor.withAlpha((0.5 * 255) ~/ 1)
-                          : AppColors.darkGray.withAlpha((0.5 * 255) ~/ 1),
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  '${match.goals.away ?? 0}',
-                  style: TextStyle(
-                    color: isDarkMode
-                        ? AppColors.whiteColor
-                        : AppColors.darkGray,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          if (isLive)
-            Container(
-              margin: const EdgeInsets.only(top: 8),
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              decoration: BoxDecoration(
-                color: AppColors.redColor,
-                borderRadius: BorderRadius.circular(4),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    width: 6,
-                    height: 6,
-                    decoration: const BoxDecoration(
-                      color: AppColors.whiteColor,
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                  const SizedBox(width: 4),
-                  Text(
-                    '${match.fixture.status.elapsed}\'',
-                    style: const TextStyle(
-                      color: AppColors.whiteColor,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildMatchInfo(Response match, bool isDarkMode) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: AppThemeConsts.padding12sm,
-        vertical: AppThemeConsts.padding8xs,
-      ),
-      decoration: BoxDecoration(
-        color: isDarkMode
-            ? AppColors.primary.withAlpha((0.1 * 255) ~/ 1)
-            : AppColors.lightGreen,
-        borderRadius: BorderRadius.circular(AppThemeConsts.radius8sm),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.location_on_outlined,
-            size: 16,
-            color: isDarkMode
-                ? AppColors.primary.withAlpha((0.9 * 255) ~/ 1)
-                : AppColors.primary,
-          ),
-          const SizedBox(width: 4),
-          Flexible(
-            child: Text(
-              match.fixture.venue.name ?? 'Unknown Venue',
-              style: TextStyle(
-                color: isDarkMode
-                    ? AppColors.primary.withAlpha((0.9 * 255) ~/ 1)
-                    : AppColors.primary,
-                fontSize: 12,
-                fontWeight: FontWeight.w500,
-              ),
-              overflow: TextOverflow.ellipsis,
-            ),
           ),
         ],
       ),
